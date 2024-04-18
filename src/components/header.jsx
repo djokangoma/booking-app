aae85d3a20ae56cee431911826d9e7c5bb591750
Kangoma <djokangoma@gmail.com>
2024-04-18 16:15:10 +0100

    feat add section component where will be my outlet

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,20 @@
-import HeaderNnav from "./header-nav";
-import Logo from "./logo";
+
 
 
 export default function Header() {
   return (
-     <div>
-      <Logo/>
-      <HeaderNnav/>
+    <header className="flex justify-between w-full">
+      <h1>logo</h1>
+      <div className="flex gap-5 list-none">
+        <li>home</li>
+        <li>rooms</li>
+        <li>pages</li>
+        <li>contact</li>
+      </div>
+      <div className="flex gap-5">
       <button>Sign in</button>
       <button>Sign up</button>
-     </div>
+      </div>
+     </header>
   )
 }
