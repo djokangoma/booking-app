1dee8378a8fc88cd558e98b832f5d5b02e863efe
djo kangoma <djokangoma@gmail.com>
2024-04-19 21:33:49 +0100

    add componenent layout where will be my outlet

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <div className="flex justify-between  w-30px h-28 bg-[#FAF9F5] ">
         <div className=" ">
           <h2>PLACE</h2>
-          <input type="text" className="block text-gray-900" placeholder="where are you going?" /> 
+          <input type="text" className="block text-gray-900 focus:" placeholder="where are you going?" /> 
         </div>
         <div >
         <button  className="text-[#4d4d4d] border-2  w-40 h-12 ">where are you going?</button>
