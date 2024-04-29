50a79a8e5da0879dea055dc8f2190372fa7f2635
djo kangoma <djokangoma@gmail.com>
2024-04-29 13:22:40 +0100

    feat: config my hero section button add giving a style

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -5,9 +5,11 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { format } from "date-fns";
+import { FaPerson } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 export default function HeroSection() {
-  const [openDate, setOpenDate]= useState(false);
+  const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -15,6 +17,28 @@ export default function HeroSection() {
       key: "selection",
     },
   ]);
+
+  const [openOptions, setOpenOptions] = useState();
+  const [options, setOptions] = useState({
+    adulte: 1,
+    children: 0,
+    room: 1,
+  });
+
+  const navigate = useNavigate();
+
+  const handeleOption = (name, operation) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
+
+  const handeleSearch = () => {
+    navigate("/hotels");
+  };
 
   return (
     <div className="relative flex items-end justify-center">
@@ -39,39 +63,104 @@ export default function HeroSection() {
           <h2 className="text-center">DATE</h2>
           <div className="relative mt-1 px-3 py-2 bg-white border shadow-sm bordel-black-300 h-10 w-60 cursor-pointer ">
             <FaRegCalendarAlt className="absolute  right-2 " />
-            <span onClick={()=>setOpenDate(!openDate)} className="absolute bottom-2 ">
+            <span
+              onClick={() => setOpenDate(!openDate)}
+              className="absolute bottom-2 "
+            >
               {`${format(date[0].startDate, "MM/dd/yyyy")} 
-              to ${format( date[0].endDate, "MM/dd/yyyy" )}`}
+              to ${format(date[0].endDate, "MM/dd/yyyy")}`}
             </span>
-            {openDate && <DateRange
-              editableDateInputs={true}
-              onChange={(item) => setDate([item.selection])}
-              moveRangeOnFirstSelection={false}
-              ranges={date}
-              className="absolute top-8 left-0"
-            />}
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="absolute top-8 left-0"
+              />
+            )}
           </div>
         </div>
 
         <div>
-          <h2 className="text-center">ROOMS</h2>
-          <input
-            type="text"
-            className="mt-1 px-3 py-2 bg-white border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block w-full rounded-md sm:text-sm focus:ring-1"
-            placeholder="where are you going?"
-          />
-          {/* <span>2 adults 2 children 1 room</span> */}
+          <h2 className="text-center">GUESTS & ROOMS</h2>
+          <div className="relative mt-1 px-3 py-2 bg-white border shadow-sm bordel-black-300 h-10 w-60 cursor-pointer ">
+            <FaPerson className="absolute right-1 " />
+            <span onClick={() => setOpenOptions(!openOptions)}>
+              {` ${options.adulte} adulte . ${options.children} children . ${options.room} room`}{" "}
+            </span>
+          </div>
+
+          {openOptions && (
+            <div className="absolute -bottom-17 bg-white w-60 ">
+              <div className="flex justify-around m-1">
+                <span>Adulte</span>
+                <div className="flex items-center gap-2 ">
+                  <button
+                    disabled={options.adulte <= 1}
+                    onClick={() => handeleOption("adulte", "d")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    -
+                  </button>
+                  <span>{options.adulte} </span>
+                  <button
+                    onClick={() => handeleOption("adulte", "i")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex justify-around  m-2">
+                <span>children</span>
+                <div className="flex items-center gap-2 ">
+                  <button
+                    disabled={options.children <= 0}
+                    onClick={() => handeleOption("children", "d")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    -
+                  </button>
+                  <span>{options.children} </span>
+                  <button
+                    onClick={() => handeleOption("children", "i")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex justify-around  m-2">
+                <span>Room</span>
+                <div className="flex items-center gap-2 ">
+                  <button
+                    disabled={options.room <= 1}
+                    onClick={() => handeleOption("room", "d")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    -
+                  </button>
+                  <span>{options.room} </span>
+                  <button
+                    onClick={() => handeleOption("room", "i")}
+                    className="bg-white size-8 border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block sm:text-sm focus:ring-1 "
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        <div>
-          <h2 className="text-center">GUESTS</h2>
-          <input
-            type="text"
-            className="mt-1 px-3 py-2 bg-white border shadow-sm border-black-300 placeholder-black- focus:outline-none focus:border-[#000] focus:ring-[#000] block w-full rounded-md sm:text-sm focus:ring-1"
-            placeholder="where are you going?"
-          />
-        </div>
+
         <div className="mt-5">
-          <button className="mt-1 px-3 py-2 bg-white border shadow-sm bordel-black-300 block w-full rounded-md ">
+          <button
+            className="mt-1 px-3 py-2 bg-[#f07857] text-white border shadow-sm bordel-black-300 block w-full rounded-md "
+            onClick={handeleSearch}
+          >
             BOOK NOW
           </button>
         </div>
