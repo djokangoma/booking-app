58bb1b037f02da5461fd8e41249e846af1c1cfb6
djo kangoma <djokangoma@gmail.com>
2024-04-29 16:29:17 +0100

    feat: added datajson where will be saome hotel

--- src/components/hotel.jsx
+++ src/components/hotel.jsx
@@ -1,21 +1,18 @@
 
-// export default function Hotel({
-//   img,
-//   nomDeLhotel,
-//   ville,
-//   prix,
-//   address
-// }) {
-//   return (
-//     <div>
-//       <div>
-//         <img src={img} alt="image de l'hotel" />
-//       </div>
-//       <div><h2>{nomDeLhotel} </h2></div>
-//       <h3>{ville}</h3>
-//       <p>{prix} </p>
-//       <p>{address} </p>
-      
-//     </div>
-//   )
-// }
+ export default function Hotel({
+  hotelImg,
+  hotelName,
+  hotelPlace,
+  hotelAdress
+ }) {
+   return (
+     <div>
+       <div>
+         {hotelImg}
+       </div>
+       <h2>{hotelName} </h2>
+       <h3>{hotelPlace} </h3>
+       <p>{hotelAdress} </p>
+     </div>
+   )
+ }
