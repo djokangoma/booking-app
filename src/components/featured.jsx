c1395b9f4acd689b320df2ca1fb0d97ce630c1ba
djo kangoma <djokangoma@gmail.com>
2024-05-03 19:12:55 +0100

    feat: add component featuredProperties.js

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,62 +1,85 @@
-
 export default function Featured() {
   return (
-    <div className="flex">
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div className="w-full max-w-screen-lg flex justify-center gap-20 ">
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden ">
+          <img
+            src="https://i.ibb.co/z7fKhtW/depositphotos-359757290-stock-photo-kinshasa-congo-january-2020-view.jpg"
+            alt="vile de kinshasa"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Kinshasa</h2>
           <h3>5 properties</h3>
         </div>
       </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden">
+          <img
+            src="https://i.ibb.co/VqXPFKz/t-l-chargement.jpg"
+            alt="ville de Lubumbashi"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Lubumbashi</h2>
           <h3>5 properties</h3>
         </div>
       </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden">
+          <img
+            src="https://i.ibb.co/bLfzkNn/t-l-chargement.jpg"
+            alt="ville de bandundu"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Bandundu</h2>
           <h3>5 properties</h3>
         </div>
       </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden">
+          <img
+            src="https://i.ibb.co/fFd5CM2/t-l-chargement-2.jpg"
+            alt="ville de Matadi"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Matadi</h2>
           <h3>5 properties</h3>
         </div>
       </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden">
+          <img
+            src="https://i.ibb.co/hZkh2ZS/t-l-chargement.jpg"
+            alt=" ville de Bukavu"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Bukavu</h2>
           <h3>5 properties</h3>
         </div>
       </div>
 
-      <div>
-        <div>
-          <img src="" alt="" />
+      <div className="relative ">
+        <div className="w-full object-cover rounded-md overflow-hidden">
+          <img
+            src="https://i.ibb.co/Yfj95Tf/t-l-chargement-1.jpg"
+            alt="ville de Goma"
+            className="size-250"
+          />
         </div>
-        <div className="featuredTitles">
+        <div className="absolute top-5 text-white left-7 ">
           <h2>Goma</h2>
           <h3>5 properties</h3>
         </div>
---
+++ src/components/featuredProperties.jsx
@@ -0,0 +1,56 @@
+export default function FeaturedProperties() {
+  return (
+    <div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+      
+      <div>
+        <img src="" alt="" />
+        <span>Hotel Golf Coast</span>
+        <span> Kinshasa</span>
+        <span>starting from $120</span>
+      </div>
+
+    </div>
+  );
+}
