0ce52de3e55702006aecff6d5608edef0fb86422
djo kangoma <djokangoma@gmail.com>
2024-05-06 00:29:02 +0100

    style: add style to carform

--- src/pages/hotels.jsx
+++ src/pages/hotels.jsx
@@ -11,22 +11,22 @@ export default function Hotels() {
   const [options, setOptions] = useState(location.state.options);
 
   return (
-    <div className="flex justify-center mt-20 ">
+    <div className="flex justify-center mt-20 mb-20">
       <div className="w-full max-w-5xl flex gap-20 ">
-        <div className="flex-1 bg-[#febb02] p-1 rounded-xl sticky top-10">
-          <h1 className="text-sm mb-10">Search</h1>
+        <div className="flex-1 bg-[#F07857] p-1 rounded-xl sticky top-10">
+          <h1 className="text-sm mb-10 text-[#FAF9F5] ">Search</h1>
           <div className="flex flex-col gap-5 mb-10">
-            <label className="tx-xs ">Destination</label>
+            <label className="tx-xs text-[#FAF9F5] ">Destination</label>
             <input
               type="text"
               placeholder={destination}
-              className="h-7 border-none p-5"
+              className="h-7 border-none p-5 bg-[#F0F0F0] text-black text-center "
             />
           </div>
           <div className="flex flex-col gap-5 mb-10">
-            <label className="tx-xs ">Check-in Date</label>
+            <label className="tx-xs text-[#FAF9F5]">Check-in Date</label>
             <span
-              className="h-28 p-5 bg-orange-400 flex items-center cursor-pointer"
+              className="h-28 p-5  flex items-center cursor-pointer text-[#FAF9F5]"
               onClick={() => setOpenDate(!openDate)}
             >
               {`${format(date[0].startDate, "MM/dd/yyyy")} 
@@ -40,63 +40,68 @@ export default function Hotels() {
               />
             )}
           </div>
-          <div className="flex flex-col gap-5 mb-10">
+          <div className="flex flex-col gap-5 mb-10 text-[#FAF9F5]">
             <label>Options</label>
             <div className="lsoptions p-10">
-              <div className="lsoptionitm flex justify-between mb-10 text-red-400 tx-xs   ">
-                <span className="lsoptiontext">
+              <div className="lsoptionitm flex justify-between mb-10  tx-xs   ">
+                <span className="lsoptiontext text-[#FAF9F5]">
                   Min price <small>par night</small>
-                  <input type="number" className="lsoptioninput w-20" />
+                  <input
+                    type="number"
+                    className="lsoptioninput w-20 bg-[#F0F0F0] text-black text-center"
+                  />
                 </span>
               </div>
-              <div className="lsoptionitm flex justify-between mb-10 text-red-400 tx-xs">
-                <span className="lsoptiontext">
+              <div className="lsoptionitm flex justify-between mb-10 text[#DEDEDE ] 400 tx-xs">
+                <span className="lsoptiontext text-[#FAF9F5]">
                   Max price <small>par night</small>
-                  <input type="number" className="lsoptioninput  w-20" />
+                  <input
+                    type="number"
+                    className="lsoptioninput  w-20 bg-[#F0F0F0] text-black text-center"
+                  />
                 </span>
               </div>
               <div className="lsoptionitm flex justify-between mb-10 text-red-400 tx-xs">
-                <span className="lsoptiontext">
+                <span className="lsoptiontext text-[#FAF9F5]">
                   Adult
                   <input
                     type="number"
                     min={1}
-                    className="lsoptioninput  w-20"
+                    className="lsoptioninput  w-20 bg-[#F0F0F0] text-black text-center"
                     placeholder={options.adulte}
                   />
                 </span>
               </div>
               <div className="lsoptionitm flex justify-between mb-10 text-red-400 tx-xs">
-                <span className="lsoptiontext">
+                <span className="lsoptiontext text-[#FAF9F5]">
                   Children
                   <input
                     type="number"
                     min={0}
-                    className="lsoptioninput  w-20"
+                    className="lsoptioninput  w-20 bg-[#F0F0F0] text-black text-center "
                     placeholder={options.children}
                   />
                 </span>
               </div>
               <div className="lsoptionitm flex justify-between mb-10 text-red-400 tx-xs">
-                <span className="lsoptiontext">
+                <span className="lsoptiontext text-[#FAF9F5]">
                   Room
                   <input
                     type="number"
                     min={1}
-                    className="lsoptioninput w-20 "
+                    className="lsoptioninput w-20 bg-[#F0F0F0] text-black text-center "
                     placeholder={options.room}
                   />
                 </span>
               </div>
             </div>
           </div>
-          <div className="bg-[#f07857] p-10 text-white border-none w-full font-medium cursor-pointer">
+          <div className="bg-[#191970] p-10 text-white border-none w-full font-medium cursor-pointer">
             {" "}
             Search
           </div>
         </div>
-        <div className="flex-3">
-        </div>
+        <div className="flex-3"></div>
       </div>
     </div>
   );
