a38cb2875d42a07dafcf019282c79193b2f0737c
djo kangoma <djokangoma@gmail.com>
2024-05-10 13:23:12 +0100

    style: style footer

--- src/components/footer-about.jsx
+++ src/components/footer-about.jsx
@@ -1,13 +1,14 @@
-
-
 export default function FooterAbout() {
   return (
     <div>
-        <h2 className="text-[#000] font-medium ">About</h2>
-        <p  className=" text-xs text-[#4d4d4d] m-4">Welcome to Alloggio, where comfort is everything. Beautiful room presentations <br />
-        straightforward booking & reservation options, & a whole lot more awaits here.
-        </p>,
-
+      <h2 className="text-[#000] font-medium text-center ">About</h2>
+      <p className=" text-xs text-[#4d4d4d] m-4">
+        Welcome to Alloggio, where comfort is everything. Beautiful room
+        presentations <br />
+        straightforward booking & reservation options, & a whole lot more awaits
+        here.
+      </p>
+      ,
     </div>
-  )
+  );
 }
--- src/components/footer-social.jsx
+++ src/components/footer-social.jsx
@@ -1,11 +1,12 @@
-
 export default function FooterSocial() {
   return (
     <div>
       <div>
-      <h2 className="text-[#000] font-medium ">Get Social</h2>
-        <p  className=" text-xs  text-[#4d4d4d] m-4 ">Follow us on social media and keep in touch <br />
-           with Alloggio.</p>
+        <h2 className="text-[#000] font-medium text-center ">Get Social</h2>
+        <p className=" text-xs  text-[#4d4d4d] m-4 ">
+          Follow us on social media and keep in touch <br />
+          with Alloggio.
+        </p>
       </div>
       <div className="flex gap-4 ">
         <img src="https://i.ibb.co/CbCG1kv/instagram.png" alt="instagram" />
@@ -13,5 +14,5 @@ export default function FooterSocial() {
         <img src="https://i.ibb.co/m6Jx4TX/facebook.png" alt="facebook" />
       </div>
     </div>
-  )
+  );
 }
