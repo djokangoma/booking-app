24c43ff8eb3b2bf44bfffe638793b5d742432c6b
djo kangoma <djokangoma@gmail.com>
2025-05-07 16:33:49 +0200

    feat: add folder routes were will be my routes

--- api/server.js
+++ api/server.js
@@ -1,8 +1,32 @@
 const express = require( "express");
 const app = express();
 const dotenv =require("dotenv");
+const mongoose  = require("mongoose");
 dotenv.config()
 
-app.listen(8800, ()=>{
-    console.log("connect to backend!!")
+
+const  connect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("connect to mongo db");
+        
+    } catch(error) {
+        throw error
+    }
+}
+
+mongoose.connection.on("disconnected",()=>
+console.log("mongo db disconnected")
+)
+mongoose.connection.on("connected",()=>
+console.log("mongo db connected")
+)
+app.get("/", (req,res)=>{
+    res.send("i love u joe kangoma")
 })
+
+
+app.listen(8800, ()=>{
+    connect()
+    console.log("connect to backend!!")
+}) 
